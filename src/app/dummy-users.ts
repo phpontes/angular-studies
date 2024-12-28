3690d183180222e19ea4e87fa34516c95a899874
phpontes <ppontes95@hotmail.com>
2024-12-28 11:30:00 -0300

    Preparing User Data To Output Dynamic Content

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Jasmim Uóxintom',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Emília Tompisom',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Marcos Jhonsom',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Deivide Miler',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Priscila Patel',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Aroldo Singh',
+    avatar: 'user-6.jpg',
+  },
+];
