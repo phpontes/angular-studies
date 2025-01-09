544480ecef21fb2fef7421c345cb7312c5d3fd63
phpontes <ppontes95@hotmail.com>
2025-01-09 07:34:16 -0300

    More Service Usage & Alternative Dependency Injection Mechanism

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -31,12 +31,7 @@ export class TasksComponent {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false;
-  }
-
-  onAddTask(taskData: NewTaskData) {
-
+  onCloseAddTask() {
     this.isAddingTask = false;
   }
 }
